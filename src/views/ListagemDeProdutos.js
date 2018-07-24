25a28d5d343127f48fbef4c76a74986625124150
Fernando Alcionei <fernandoalcioneidesouza@gmail.com>
2018-07-24 10:05:36 -0300

    Adicionando tab listagem de produtos

--- src/views/ListagemDeProdutos.js
+++ src/views/ListagemDeProdutos.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View,StyleSheet } from 'react-native';
+
+import Produto from '../components/Produto';
 
 export default class ListagemDeProdutos extends Component{
     render(){
         return(
             <View style={{flex:1}}>
-                <Text>
-                    Produtos....
-                </Text>
+                <Produto />
             </View>
         )
     }
